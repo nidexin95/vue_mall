0c523fd3c2753e44738e4fe2db588136f8cd51b3
Ken <578720530@qq.com>
2020-09-16 15:06:06 +0800

    新增导航栏组件

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>首页</div>
+  <div>
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "@/components/common/navbar/NavBar";
 export default {
-    name : 'Home'
-}
+  name: "Home",
+  components: {
+    NavBar,
+  },
+};
 </script>
 
 <style>
-
+.home-nav {
+  background: #d4237a;
+  color: #ffffff;
+}
 </style>
